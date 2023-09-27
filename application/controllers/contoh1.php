4b1a10331417a911adfed71adfd4f489787eaa7c
EgiSitorus0852 <egisitorus140@ganail.com>
2023-09-27 15:42:02 +0700

    menambahkan contoh1

---
+++ application/controllers/contoh1.php
@@ -0,0 +1,11 @@
+<?php
+class Contoh extends CI_Controller
+{
+    public function index()
+    {
+        echo "<h1>perkenalkan</h1>";
+        echo"Nama saya Egi Danico Saptian Sitorus
+            saya tinggal di Bekasi
+            olahraga yang saya sukai adalah berenang";
+    }
+}
